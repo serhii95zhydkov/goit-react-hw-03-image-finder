1509c572a437ad93c92a9f7adb40cb51ef29b628
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-01-29 23:30:18 +0200

    Create ImageGalleryItem.jsx

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,29 @@
+import styles from './ImageGalleryItem.module.css';
+import PropTypes from 'prop-types';
+
+export const ImageGalleryItem = ({
+  id,
+  tags,
+  webformatURL,
+  largeImageURL,
+  openModal,
+}) => {
+  return (
+    <li
+      key={id}
+      className={styles.galleryItem}
+      onClick={() => {
+        openModal(largeImageURL);
+      }}
+    >
+      <img className={styles.imageGalleryItem} src={webformatURL} alt={tags} />
+    </li>
+  );
+};
+
+ImageGalleryItem.propTypes = {
+  tags: PropTypes.string.isRequired,
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  openModal: PropTypes.func.isRequired,
+};
